3eb3c88562a159dbe4814d9d8ec6fe730e84d43a
Fernando José <fernandogjose@gmail.com>
2020-06-29 03:46:04 -0300

    ajuste no responsivo

--- Src/Fgj.Cqrs.React/web/src/components/user/user-table.component.js
+++ Src/Fgj.Cqrs.React/web/src/components/user/user-table.component.js
@@ -20,70 +20,76 @@ export class UserTable extends Component {
 
         return (
             <div className="container margin-top-10">
-                <table className="table">
+                <div className="row">
+                    <div className="col-md-12">
+                        <div className="table-responsive">
+                            <table className="table">
 
-                    {/* Head */}
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>E-mail</th>
-                            <th>Type</th>
-                            <th>CpfCnpj</th>
-                            <th><Link to={"/user-create"} className="btn btn-sm btn-primary">Create</Link></th>
-                        </tr>
-                    </thead>
+                                {/* Head */}
+                                <thead>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>E-mail</th>
+                                        <th>Type</th>
+                                        <th>CpfCnpj</th>
+                                        <th><Link to={"/user-create"} className="btn btn-sm btn-primary">Create</Link></th>
+                                    </tr>
+                                </thead>
 
-                    {/* List */}
-                    <tbody>
-                        {users && users.map((user, index) => (
-                            <tr key={index}>
-                                <td>{user.name}</td>
-                                <td>{user.email}</td>
-                                <td>{user.type}</td>
-                                <td>{user.cpfCnpj}</td>
-                                <td>
-                                    <Link to={"/user-edit/" + user.guidUser} className="btn btn-sm btn-success">Edit</Link>&nbsp;
+                                {/* List */}
+                                <tbody>
+                                    {users && users.map((user, index) => (
+                                        <tr key={index}>
+                                            <td>{user.name}</td>
+                                            <td>{user.email}</td>
+                                            <td>{user.type}</td>
+                                            <td>{user.cpfCnpj}</td>
+                                            <td>
+                                                <Link to={"/user-edit/" + user.guidUser} className="btn btn-sm btn-success">Edit</Link>&nbsp;
                                     <button className="btn btn-sm btn-danger" onClick={() => userDelete(user.guidUser, user.guidProfile)}>Delete</button>
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
 
-                    {/* Pagination */}
-                    <tfoot>
-                        <tr>
-                            <td colSpan="5">
-                                <nav>
-                                    <ul className="pagination justify-content-center">
-                                        <li className="page-item">
-                                            {
-                                                currentPage === 1
-                                                    ? <button className="page-link">Previous</button>
-                                                    : <button className="page-link" onClick={() => prevPage()}>Previous</button>
-                                            }
-                                        </li>
+                                {/* Pagination */}
+                                <tfoot>
+                                    <tr>
+                                        <td colSpan="5">
+                                            <nav>
+                                                <ul className="pagination justify-content-center">
+                                                    <li className="page-item">
+                                                        {
+                                                            currentPage === 1
+                                                                ? <button className="page-link">Previous</button>
+                                                                : <button className="page-link" onClick={() => prevPage()}>Previous</button>
+                                                        }
+                                                    </li>
 
-                                        {
-                                            pageNumbers.map(num => (
-                                                <li className="page-item" key={num}>
-                                                    <button onClick={() => paginate(num)} className="page-link">{num}</button>
-                                                </li>
-                                            ))
-                                        }
+                                                    {
+                                                        pageNumbers.map(num => (
+                                                            <li className="page-item" key={num}>
+                                                                <button onClick={() => paginate(num)} className="page-link">{num}</button>
+                                                            </li>
+                                                        ))
+                                                    }
 
-                                        <li className="page-item">
-                                            {
-                                                currentPage === pagesTotal
-                                                    ? <button className="page-link">Next</button>
-                                                    : <button className="page-link" onClick={() => nextPage()}>Next</button>
-                                            }
-                                        </li>
-                                    </ul>
-                                </nav>
-                            </td>
-                        </tr>
-                    </tfoot>
-                </table>
+                                                    <li className="page-item">
+                                                        {
+                                                            currentPage === pagesTotal
+                                                                ? <button className="page-link">Next</button>
+                                                                : <button className="page-link" onClick={() => nextPage()}>Next</button>
+                                                        }
+                                                    </li>
+                                                </ul>
+                                            </nav>
+                                        </td>
+                                    </tr>
+                                </tfoot>
+                            </table>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
