d0d436f32929abb265e839229fce06ee69546e5e
Fernando José <fernando.jose@viavarejo.com.br>
2020-07-14 15:44:00 -0300

    inclusão do ico

--- Src/Fgj.Cqrs.React/nextjs-blog/components/headtag.js
+++ Src/Fgj.Cqrs.React/nextjs-blog/components/headtag.js
@@ -8,7 +8,7 @@ class HeadTag extends Component {
         return (
             <Head>
                 <title>{title}</title>
-                <link rel="icon" href="/images/favicon.ico" />
+                <link rel="icon" href="/favicon.ico" />
 
                 {/* bootstrap */}
                 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossOrigin="anonymous"></link>
