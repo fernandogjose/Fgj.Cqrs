5f7845a4f534be6a272ec45cb21149eeba71e70b
Fernando José <fernandogjose@gmail.com>
2020-07-14 00:23:32 -0300

    ajuste no layout

--- Src/Fgj.Cqrs.React/nextjs-blog/components/layout.js
+++ Src/Fgj.Cqrs.React/nextjs-blog/components/layout.js
@@ -1,14 +1,19 @@
+import React, { Component } from 'react';
 import HeadTag from "./headtag";
 import Header from "./header";
 import Footer from "./footer";
 
-function Layout({ children, title }) {
-    return <div>
-        <HeadTag title={title}></HeadTag>
-        <Header></Header>
-        {children}
-        <Footer></Footer>
-    </div>
+class Layout extends Component {
+    render() {
+        const { children, title } = this.props;
+
+        return <div>
+            <HeadTag title={title}></HeadTag>
+            <Header></Header>
+            {children}
+            <Footer></Footer>
+        </div>
+    }
 }
 
 export default Layout
